0f6ace04f011dba22c446adea31a4c58c5361c6a
khushal <kushalnaik.me@gmail.com>
2022-12-03 14:14:37 +0530

    change3

--- js/navbar.js
+++ js/navbar.js
@@ -8,3 +8,12 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "-50px";
   }
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
